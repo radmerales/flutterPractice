9f712ce2a546e80cb18641c460eb4ca79030901d
radmerales <radmerales12345@gmail.com>
2021-11-08 21:13:58 +0800

    Moved the Scaffold Widget to its own Stateless widget. This enables hot reload.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Penk',),
-      centerTitle: true,
-      backgroundColor: Colors.pink[500],
-    ),
-    body: Center(
-      child: Text(
-        'hello ninjas',
-        style: TextStyle(
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-      )),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Tara'),
-      backgroundColor: Colors.pink[400],
-      onPressed: (){},
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Penk',),
+        centerTitle: true,
+        backgroundColor: Colors.pink[500],
+      ),
+      body: Center(
+        child: Text(
+            'Sheeesh',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Tara'),
+        backgroundColor: Colors.pink[400],
+        onPressed: (){},
+      ),
+    );
+  }
+}
