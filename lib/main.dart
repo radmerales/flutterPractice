d7f1e83f4646af68a4e2dba34c27c551c3c31cfe
radmerales <radmerales12345@gmail.com>
2021-11-08 20:58:46 +0800

    End of simple app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("Hello World"),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Penk',),
+      centerTitle: true,
+      backgroundColor: Colors.pink[500],
+    ),
+    body: Center(
+      child: Text(
+        'hello ninjas',
+        style: TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      )),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('Tara'),
+      backgroundColor: Colors.pink[400],
+      onPressed: (){},
+    ),
+  ),
 ));
