0698c1010a6ff33a6743271d14f7b756c7bb9d45
radmerales <radmerales12345@gmail.com>
2021-11-09 14:32:08 +0800

    Sample App Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -9,23 +11,116 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Penk',),
-        centerTitle: true,
-        backgroundColor: Colors.pink[500],
+        title: Container(
+          child: Text('JobFinder'),
+          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+        ),
+        backgroundColor: Colors.black,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
       ),
-      body: Center(
-        child: Text(
-            'Sheeesh',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            )),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text('Tara'),
-        backgroundColor: Colors.pink[400],
-        onPressed: (){},
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                flex: 6,
+                child: Container(
+                  color: Colors.blueGrey[50],
+                ),
+              )
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(30,0,0,0),
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "Hello Person.\n",
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                      TextSpan(
+                        text: "Find the right job for you.",
+                        style: TextStyle(fontWeight: FontWeight.bold, height: 1.4),
+                      )
+                    ]
+                  )
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      width: 400.0,
+                      height: 160.0,
+                      child: Column(
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(color: Colors.black),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: "Data Scientist\n",
+                                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, height: 1.4),
+                                    ),
+                                    TextSpan(
+                                      text: "LinkedIn",
+                                      style: TextStyle(color: Colors.yellow[800], fontWeight: FontWeight.bold, fontSize: 15, height: 1.5),
+                                    ),
+                                  ]
+                                  )
+                              ),
+                            ],
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(0, 5, 5, 10),
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.fromLTRB(9,9,9,9),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black54,
+                                    borderRadius: BorderRadius.all(Radius.circular(20))
+                                  ),
+                                  child: Text('Sample', style: TextStyle(color: Colors.white)),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                    ),
+                    Container(
+                      color: Colors.blue,
+                    ),
+                  ],
+                )
+              )
+            ],
+          )
+        ],
       ),
     );
   }
 }
+
